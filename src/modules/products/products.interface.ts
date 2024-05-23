fe9c42503bc4c671ecfcfe530a5c1d8353f8d0d7
Afia Nahian Chowdhury <afia000nahian@gmail.com>
2024-05-23 20:16:59 +0600

    first commit

---
+++ src/modules/products/products.interface.ts
@@ -0,0 +1,18 @@
+export type TVariant = {
+    type: string;
+    value: string;
+}
+export type TInventory = {
+    quantity: number;
+    inStock: boolean;
+}
+export type TProduct = {
+    _id?: string;
+    name: string;
+    description: string;
+    price: number;
+    category: string;
+    tags: string[];
+    variants: [TVariant];
+    inventory: TInventory
+}
