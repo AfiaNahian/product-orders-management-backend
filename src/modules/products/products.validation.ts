b07afccaa05f35f71814a368068c01d40dbc9308
Afia Nahian Chowdhury <afia000nahian@gmail.com>
2024-05-30 01:01:27 +0600

    products complete

--- src/modules/products/products.validation.ts
+++ src/modules/products/products.validation.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 
 const TVariantValidationSchema = z.object({
     type: z.string().min(1, { message: 'Variant cannot be empty' }).max(20, { message: 'Variant type should not exceed 20 characters' }),
-    value: z.string().min(1, { message: 'Provide some value information' }).max(20, { message: 'Variant value should not exceed 20 characters' }),
+    value: z.string().min(1, { message: 'Value cannot be empty' }).max(20, { message: 'Variant value should not exceed 20 characters' }),
 });
 
 // Define the TInventoryValidationSchema type
@@ -17,11 +17,11 @@ const TInventoryValidationSchema = z.object({
 // Define the ProductValidationSchema type
 const ProductValidationSchema = z.object({
     _id: z.string().optional(),
-    name: z.string().min(1, { message: "Must be 1 or more characters long" }).max(30, { message: 'Product name must not exceed 30 characters' }),
-    description: z.string().min(10, { message: "Product must have at least 5 characters long description" }).max(150, { message: "Product description should not exceed 150 characters" }),
+    name: z.string().min(1, { message: "Product name at least be 1 letter" }).max(30, { message: 'Product name can not exceed 30 letters' }),
+    description: z.string().min(10, { message: "Product at least need 10 characters long description" }).max(150, { message: "Product description should not exceed 150 characters" }),
     price: z.number().nonnegative({ message: 'Price cannot be negative' }),
     category: z.string({
-        required_error: "category for the product is required",
+        required_error: "category is required",
     }),
     tags: z.array(z.string()),
     variants: z.array(TVariantValidationSchema),
